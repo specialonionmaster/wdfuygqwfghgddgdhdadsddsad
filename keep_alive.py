8bb5a9c5752e053125231cea950be4f2955635d4
specialonionmaster <specialonionmaster@github.com>
2026-07-15 02:07:00 +0300

    фикс порта

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Бот работает!"
+    return "OK"
 
 def run():
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
